48b812f30041753cf7864f48fe23c9186a189777
ifan112 <ifan.w112@gmail.com>
2020-07-14 19:48:25 +0800

    理解服务异常

--- demo-springcloud-x/src/main/java/com/ifan112/demo/springcloud/x/TestController.java
+++ demo-springcloud-x/src/main/java/com/ifan112/demo/springcloud/x/TestController.java
@@ -2,6 +2,7 @@ package com.ifan112.demo.springcloud.x;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -27,6 +28,29 @@ public class TestController {
         respBuilder.append("X test2").append("\n");
 
         ResponseEntity<String> respEntity = restTemplate.getForEntity("http://" + yServiceId + "/test", String.class);
+        System.out.println(respEntity.getStatusCode().toString());
+
+        respBuilder.append(respEntity.getStatusCode().toString())
+                .append("\n");
+        respBuilder.append(respEntity.getBody());
+
+        return respBuilder.toString();
+    }
+
+    @GetMapping("/test3")
+    public String test3(@RequestParam Integer id) {
+        StringBuilder respBuilder = new StringBuilder();
+        respBuilder.append("X test3").append("\n");
+
+        /*
+         * restTemplate 在得到响应后，检查 Header 发现状态码是 4xx 或 5xx 时就会抛出异常
+         *
+         * 实际上，它依然得到了下面的响应内容：
+         * {"timestamp":"2020-07-14T11:29:22.789+00:00","status":500,"error":"Internal Server Error","message":"","path":"/test2"}
+         *
+         * 除非上游服务在响应的消息体中主动地返回自己的异常栈，否则下游服务无法得到上游服务异常栈是什么
+         */
+        ResponseEntity<String> respEntity = restTemplate.getForEntity("http://" + yServiceId + "/test2?id=" + id, String.class);
         System.out.println(respEntity.getStatusCode().toString());
 
         respBuilder.append(respEntity.getStatusCode().toString())
--- demo-springcloud-y/src/main/java/com/ifan112/demo/springcloud/y/TestController.java
+++ demo-springcloud-y/src/main/java/com/ifan112/demo/springcloud/y/TestController.java
@@ -1,6 +1,7 @@
 package com.ifan112.demo.springcloud.y;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,23 @@ public class TestController {
     @GetMapping("/test")
     public String test() {
         return "Y Test";
+    }
+
+    @GetMapping("test2")
+    public String test2(@RequestParam Integer id) {
+        /*
+         * 默认情况下，Spring 框架不会处理任何异常，将异常向上抛给了 Tomcat，最后返回的响应如下：
+         * HTTP/1.1 500
+         * Content-Type: application/json
+         * Connection: close
+         *
+         * {"timestamp":"2020-07-14T11:29:22.789+00:00","status":500,"error":"Internal Server Error","message":"","path":"/test2"}
+         */
+
+        if (id < 0) {
+            throw new IllegalArgumentException("id invalid.");
+        }
+
+        return "Y Test2 OK";
     }
 }
